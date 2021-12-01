1cdbfc6a59b3d118c975a485284a691fd826fa71
Elton Soares <eltons@ibm.com>
2021-12-01 02:00:12 -0300

    refactoring rabbitmq observer client

--- hkpy/hkbase/observer/clients/rabbitmqobserverclient.py
+++ hkpy/hkbase/observer/clients/rabbitmqobserverclient.py
@@ -5,9 +5,12 @@
 
 import json
 import logging
+import time
 import traceback
 import os
 import pika
+import pika.exceptions
+from threading import Thread
 
 from hkpy.hkbase.observer.clients.configurableobserverclient import ConfigurableObserverClient as ObserverClient
 from hkpy.hkbase.observer.clients.configurableobserverclient import HKBase
@@ -39,8 +42,8 @@ class RabbitMQObserverClient(ObserverClient):
         }
         self._channel = None
         self._connection = None
-        self._consumer_id = None
         self._queue_name = None
+        self._consumer_thread = None
 
     def init(self):
         try:
@@ -69,31 +72,42 @@ class RabbitMQObserverClient(ObserverClient):
 
             observer_id = self._observer_id
 
-            def callback(ch, method, properties, body):
-                try:
-                    message = json.loads(body.decode('utf-8'))
-                    if observer_id and message.get('observerId', '') == observer_id:
-                        self.notify(message.get('notification', {}))
-                    elif not observer_id:
-                        self.notify(message)
-                except Exception as e:
-                    traceback.print_exc()
-                    logging.error(e)
-
-            self._consumer_id = channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
             self._channel = channel
             self._queue_name = queue_name
             self._connection = connection
-            logging.info(f" [*] Waiting for messages in {queue_name}. To exit press CTRL+C")
-            channel.start_consuming()
+
+            def consumer():
+                logging.info(f" [*] Waiting for messages in {self._queue_name}. To exit press CTRL+C")
+                time.sleep(1)
+                while self._queue_name is not None:
+                    try:
+                        try:
+                            method, properties, body = channel.basic_get(queue=self._queue_name, auto_ack=True)
+                        except pika.exceptions.DuplicateGetOkCallback:
+                            continue
+                        except (pika.exceptions.ConnectionWrongStateError, pika.exceptions.ChannelWrongStateError):
+                            logging.info('stopping consumer')
+                            return
+                        if body is None:
+                            continue
+                        message = json.loads(body.decode('utf-8'))
+                        if observer_id and message.get('observerId', '') == observer_id:
+                            self.notify(message.get('notification', {}))
+                        elif not observer_id:
+                            self.notify(message)
+                    except Exception as e:
+                        traceback.print_exc()
+                        logging.error(e)
+                    time.sleep(1)
+            self._consumer_thread = Thread(target=consumer)
+            self._consumer_thread.start()
+
         except Exception as e:
             traceback.print_exc()
             logging.error(e)
 
     def deinit(self):
         logging.info("Deiniting observer")
-        self._channel.basic_cancel(self._consumer_id)
-        self._consumer_id = None
         logging.info('canceled AMQP consumer')
         if self._observer_id is None:
             self._channel.queue_delete(self._queue_name)
@@ -103,8 +117,9 @@ class RabbitMQObserverClient(ObserverClient):
             self.unregister_observer()
         self._channel.close()
         self._channel = None
-        self._connection.close()
         self._connection = None
+        self._consumer_thread.join()
+        self._consumer_thread = None
 
     def _parse_config(self):
         url_with_protocol = self._config['broker'].replace('amqp://', '')
